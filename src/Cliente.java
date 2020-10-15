dc92423675b3b25fc223e8d57f92953821d181e7
AND513 <72775676+AND513@users.noreply.github.com>
2020-10-15 10:29:01 -0500

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,5 @@
 /**
- * Clase
+ * Clase para modelar un contacto Cliente a partir de otra clase
  * @author Cruz Barrios Luis Alberto
  * @author Flores Molina Alfredo
  * @author Fernandez Neria Montserrat
@@ -12,7 +12,17 @@ public class Cliente extends ContactoCC {
     private String compania;
     private int extension;
     private String webpage;
-
+/**
+     * Constructor que recibe parametros
+     *
+     * @param nombre El nombre del Cliente
+     * @param telefono El telefono casa del Cliente
+     * @param celular El celular del Cliente
+     * @param correo El correo del Cliente
+     * @param compania La compania del Cliente
+     * @param extension La extension del Cliente
+     * @param webpage La pagina web del Cliente
+     */
     public Cliente(String nombre, int telefono, int celular, String correo, String compania, int extension,
             String webpage) {
         super(nombre, telefono, celular, correo);
@@ -20,31 +30,61 @@ public class Cliente extends ContactoCC {
         this.extension = extension;
         this.webpage = webpage;
     }
-
+/**
+     * Metodo para obtener la compania del Cliente
+     *
+     * @return String La compania del Cliente
+     */
     public String getCompania() {
         return compania;
     }
 
+/**
+     * Metodo para obtener la extension del Cliente
+     *
+     * @return int La extension del Cliente
+     */
     public int getExtension() {
         return extension;
     }
 
+/**
+     * Metodo para obtener la pagina web del Cliente
+     *
+     * @return String La pagina web del Cliente
+     */
     public String getWebpage() {
         return webpage;
     }
-
+/**
+     * Metodo para establecer la compania del Cliente
+     *
+     * @param compania La compania del Cliente
+     */
     public void setCompania(String compania) {
         this.compania = compania;
     }
-
+/**
+     * Metodo para establecer la extension del Cliente
+     *
+     * @param extension La extension del Cliente
+     */
     public void setExtension(int extension) {
         this.extension = extension;
     }
-
+/**
+     * Metodo para establecer la pagina web del Cliente
+     *
+     * @param compania La pagina web del Cliente
+     */
     public void setWebpage(String webpage) {
         this.webpage = webpage;
     }
-
+/**
+     * Metodo para imprimir un contacto Cliente  como una cadena de caracteres
+     *
+     * @return String El contacto Cliente en formato de cadena de caracter
+     */
     @Override
     public String toString() {
         return super.toString() + "\nCompania: " + compania + "Extension: " + extension + "Pagina web: " + webpage;
