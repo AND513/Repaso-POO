4b8b540ef27cc482456b5dc4ac63812d64e4013f
AND513 <72775676+AND513@users.noreply.github.com>
2020-10-15 09:20:42 -0500

    Update Contacto.java

--- src/Contacto.java
+++ src/Contacto.java
@@ -1,5 +1,5 @@
 /**
- * Clase
+ * Clase para definir un Contacto
  * @author Cruz Barrios Luis Alberto
  * @author Flores Molina Alfredo
  * @author Fernandez Neria Montserrat
@@ -12,27 +12,50 @@ public class Contacto {
     private String nombre;
     private int telefono;
 
+     /**
+   * Constructor que recibe parámetros
+   * @param nombre El nombre del Contacto
+   * @param telefono El telefono del Contacto
+      */
     public Contacto(String nombre, int telefono) {
         this.nombre = nombre;
         this.telefono = telefono;
     }
 
+    /**
+   * Método para obtener el nombre del Contacto
+   * @return String El nombre del Contacto
+   */
     public String getNombre() {
         return nombre;
     }
-
+    
+    /**
+   * Método para obtener el telefono del Contacto
+   * @return int El telefono del Contacto
+   */
     public int getTelefono() {
         return telefono;
     }
-
+/**
+   * Método para establecer el nombre del Contacto
+   * @param nombre El nombre del Contato
+   */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+/**
+   * Método para establecer el telefono del Contacto
+   * @param telefono El telefono del Contacto
+   */
     public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
 
+    /**
+   * Método para imprimir un Contacto como una cadena de caracteres
+   * @return String El Contacto en formato de cadena de caracter
+   */
     @Override
     public String toString() {
         return "Nombre: " + nombre + "\nTelefono: " + telefono;
